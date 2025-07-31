9630086a1e5a33ab4dd4d0376454b477a0d09d15
mehrab2231348 <mehrabbhuiyan123@gmail.com>
2025-08-01 00:02:21 +0600

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.imulatingoperationsofkaziandkaziteaestate {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.imulatingoperationsofkaziandkaziteaestate to javafx.fxml;
+    exports com.example.imulatingoperationsofkaziandkaziteaestate;
+}
