1b735bc99d9bf15c06ac56909513b2c65a11a956
mehrab2231348 <mehrabbhuiyan123@gmail.com>
2025-08-12 22:19:39 +0600

    update dashboard 2 scene switching

--- src/main/java/com/example/Simulatingoperationsofkaziandkaziteaestate/Mehrab/Dashboard2.java
+++ src/main/java/com/example/Simulatingoperationsofkaziandkaziteaestate/Mehrab/Dashboard2.java
@@ -1,7 +1,11 @@
 package com.example.Simulatingoperationsofkaziandkaziteaestate.Mehrab;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class Dashboard2
 {
@@ -13,38 +17,54 @@ public class Dashboard2
     }
 
     @javafx.fxml.FXML
-    public void salesTrendReportOnClicked(ActionEvent actionEvent) {
-    }
+    public void salesTrendReportOnClicked(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("salestrendreport.fxml"));
+        Node node = fxmlLoader.load() ;
+        dashboardBP.setCenter(node);}
 
     @javafx.fxml.FXML
-    public void applyDiscountsPromotionsButtonOnclicked(ActionEvent actionEvent) {
-    }
+    public void applyDiscountsPromotionsButtonOnclicked(ActionEvent actionEvent)throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ApplyDiscountsPromotions.fxml"));
+        Node node = fxmlLoader.load() ;
+        dashboardBP.setCenter(node);}
 
     @javafx.fxml.FXML
-    public void cancelVoidInvoiceButtonOnClicked(ActionEvent actionEvent) {
-    }
+    public void cancelVoidInvoiceButtonOnClicked(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CancelVoidInvoice.fxml"));
+        Node node = fxmlLoader.load() ;
+        dashboardBP.setCenter(node);}
 
     @javafx.fxml.FXML
-    public void salesReportButtonOnClicked(ActionEvent actionEvent) {
-    }
+    public void salesReportButtonOnClicked(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("SalesReport.fxml"));
+        Node node = fxmlLoader.load() ;
+        dashboardBP.setCenter(node);}
 
     @javafx.fxml.FXML
-    public void monthlySalesTrendReportButtonOnClicked(ActionEvent actionEvent) {
-    }
+    public void monthlySalesTrendReportButtonOnClicked(ActionEvent actionEvent)throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MonthlySalesTrendReport.fxml"));
+        Node node = fxmlLoader.load() ;
+        dashboardBP.setCenter(node);}
 
     @javafx.fxml.FXML
-    public void updateInvoiceQuantityButtonOnClicked(ActionEvent actionEvent) {
-    }
+    public void updateInvoiceQuantityButtonOnClicked(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UpdateInvoiceQuantity.fxml"));
+        Node node = fxmlLoader.load() ;
+        dashboardBP.setCenter(node);}
 
     @javafx.fxml.FXML
-    public void customerPurchaseHistoryButtonOnClicked(ActionEvent actionEvent) {
-    }
+    public void customerPurchaseHistoryButtonOnClicked(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CustomerPurchaseHistory.fxml"));
+        Node node = fxmlLoader.load() ;
+        dashboardBP.setCenter(node);}
 
     @javafx.fxml.FXML
     public void signOutButtonOnClicked(ActionEvent actionEvent) {
     }
 
     @javafx.fxml.FXML
-    public void handleReturnsAdjustmentsButtonOnClicked(ActionEvent actionEvent) {
-    }
+    public void handleReturnsAdjustmentsButtonOnClicked(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("HandleReturnsAdjustments.fxml"));
+        Node node = fxmlLoader.load() ;
+        dashboardBP.setCenter(node);}
 }
