3b1b349f0385589be0af235b66c218529d201ffe
Md. Mahedi Hasan Mahin <mahedimahinit08@gmail.com>
2025-08-14 12:42:30 +0600

    3rd update
    3rd update

---
+++ src/main/java/com/example/Simulatingoperationsofkaziandkaziteaestate/Mahin/MonitorEstateOperatorController.java
@@ -0,0 +1,38 @@
+package com.example.Simulatingoperationsofkaziandkaziteaestate.Mahin;
+
+public class MonitorEstateOperatorController
+{
+    @javafx.fxml.FXML
+    private TextField pluckedField;
+    @javafx.fxml.FXML
+    private TextField absenteeField;
+    @javafx.fxml.FXML
+    private TableColumn pluckedCol;
+    @javafx.fxml.FXML
+    private TableColumn zoneCol;
+    @javafx.fxml.FXML
+    private DatePicker activityDatePicker;
+    @javafx.fxml.FXML
+    private Button viewUpdatesBtn;
+    @javafx.fxml.FXML
+    private TableView dailyLogsTable;
+    @javafx.fxml.FXML
+    private TableColumn absenteeCol;
+    @javafx.fxml.FXML
+    private ComboBox zoneComboBox;
+    @javafx.fxml.FXML
+    private TableColumn dateCol;
+    @javafx.fxml.FXML
+    private TableColumn rainfallCol;
+    @javafx.fxml.FXML
+    private TextArea remarksArea;
+    @javafx.fxml.FXML
+    private TextField rainfallField;
+    @javafx.fxml.FXML
+    private Button saveEntryBtn;
+    @javafx.fxml.FXML
+    private TableColumn remarksCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
